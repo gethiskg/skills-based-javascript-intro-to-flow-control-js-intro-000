3931d1334de6f7c6f3e6278e2b9c8da825484d77
Ismaiilov Atai <atai2497@gmail.com>
2020-05-28 00:46:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -9,10 +9,10 @@
 // basicTeenager(111);
 function basicTeenager(age) {
   if (age >= 13 && age >= 19) {
-    return `You are a teenager!`
+    return `You are a teenager!`;
   }
 }
-basicTeenager(13)
+
 
 
 
